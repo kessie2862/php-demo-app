c549bbebc49010c5062235a9b57453b76dc6e39b
kessie <kayp3477@gmail.com>
2024-02-28 13:30:49 +0000

    Add all files

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+$books = [
+  [
+      'name' => 'Rich Dad Poor Dad',
+      'author' => 'Robert Kiyosaki',
+      'releaseYear' => 2011,
+      'purchaseUrl' => 'www.rdpd.com'
+  ],
+  [
+      'name' => 'Ego Is The Enemy',
+      'author' => 'Ryan Holiday',
+      'releaseYear' => 2000,
+      'purchaseUrl' => 'www.eite.com'
+  ],
+  [
+      'name' => 'In The Chest Of A Woman.',
+      'author' => 'Ryan Holiday',
+      'releaseYear' => 2009,
+      'purchaseUrl' => 'www.itcoaw.com'
+  ],
+];
+
+// THIS FUNCTION IS REPLACED BY PHP's array_filter BELOW.
+// function filter($items, $fn) {
+//     $filteredItems = [];
+
+//     foreach($items as $item) {
+//         if($fn($item)){
+//             $filteredItems[] = $item;
+//         }
+//     }
+
+//     return $filteredItems;
+// }
+
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['author'] === 'Robert Kiyosaki';
+});
+
+require 'index.view.php';
